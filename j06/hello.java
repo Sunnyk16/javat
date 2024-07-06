7e544842e3d10fb6b36367d4af1c2718143a00db
Sunnyk16 <kumarsunny1246@gmail.com>
2024-07-06 22:34:01 +0530

    java codes

---
+++ j06/hello.java
@@ -0,0 +1,16 @@
+// package j06;
+
+public class hello {
+    public static void main(String[] args){
+        int num1= 5;
+        float f1=3;
+        float f2= (int) num1;
+        System.out.println("hello");
+        System.out.println(num1);
+        System.out.println(f1);
+        System.out.println(f2);
+    }
+     
+    
+}
+ 
